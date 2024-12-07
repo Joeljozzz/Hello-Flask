f0351a20b2a2fb974c2f14a7db6f42887048fc86
Joel Jose <72685604+Joeljozzz@users.noreply.github.com>
2024-12-07 14:39:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud():
-    return 'Hello Cloud!'
+    return 'Hello Cloud! From Joel Jose'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
